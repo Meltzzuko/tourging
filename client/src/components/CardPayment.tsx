5e15b808d72bb2767898a712acdd594493c7cd3b
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2023-02-13 20:24:20 +0700

    fix ui payment page

--- client/src/components/CardPayment.tsx
+++ client/src/components/CardPayment.tsx
@@ -7,18 +7,18 @@ function CardPayment() {
     return(
         <div className="item">
             <div className="content">
-                <Typography style={{ fontSize: 50, textAlign: "left", fontWeight: "800" }} color='black'>การชำระเงิน</Typography>
-                <Typography style={{ fontSize: 35, textAlign: "center", fontWeight: "bold", color: "#0147AB"}}>ทัวร์เกาะสิมิลัน</Typography>
+                <Typography style={{ fontSize: 30, textAlign: "left", fontWeight: "800" }} color='black'>การชำระเงิน</Typography>
+                <Typography style={{ fontSize: 26, textAlign: "center", fontWeight: "bold", color: "#0147AB"}}>ทัวร์เกาะสิมิลัน</Typography>
                 <Box display="flex" flexDirection="column" alignItems="center">
-                    <FigureImage  style={{ borderRadius: 10,  border: "2px solid black" }} width={400} height={600} alt="171x180" src="similan.png" /> 
+                    <FigureImage  style={{ borderRadius: 10,  border: "2px solid black" }} width={300} height={300} alt="171x180" src="similan.png" /> 
                 </Box>
-                <Typography style={{fontSize : 26, textAlign : "left", paddingLeft: 75, fontWeight: "550", color: "#3c73a8" }} >
+                <Typography style={{fontSize : 20, textAlign : "left", paddingLeft: 75, fontWeight: "550", color: "#3c73a8" }} >
                     รายละเอียดการจอง
                 </Typography>
-                <Typography style={{fontSize : 26, textAlign : "left", paddingLeft: 75, fontWeight: "550", color: "#3c73a8"}} >
+                <Typography style={{fontSize : 20, textAlign : "left", paddingLeft: 75, fontWeight: "550", color: "#3c73a8"}} >
                     ประเภททัวร์ ทัวร์รายวัน
                 </Typography>
-                <Typography style={{fontSize : 26, textAlign : "left", paddingLeft: 75, fontWeight: "550", color: "#3c73a8" }} >
+                <Typography style={{fontSize : 20, textAlign : "left", paddingLeft: 75, fontWeight: "550", color: "#3c73a8" }} >
                     จำนวนผู้จอง 1 ท่าน
                 </Typography>
                 <br/>
--- client/src/page/Payment.tsx
+++ client/src/page/Payment.tsx
@@ -13,16 +13,15 @@ const PaymentPage = () => {
                 <CardPayment/>
                 <div className="item">
                     <div className="content">
-                    <Typography style={{ fontSize: 30, textAlign: "center", fontWeight: "700", marginTop: "30px" }} color='black'>สแกนเพื่อชำระเงิน</Typography>
+                    <Typography style={{ fontSize: 24, textAlign: "center", fontWeight: "700", marginTop: "10px" }} color='black'>สแกนเพื่อชำระเงิน</Typography>
                     <Box display="flex" flexDirection="column" alignItems="center">
-                        <FigureImage  style={{ borderRadius: 10,  border: "2px solid black", marginTop: "30px" }} width={170} height={175}  src="QR-code.png" /> 
+                        <FigureImage  style={{ borderRadius: 10,  border: "2px solid black", marginTop: "10px" }} width={170} height={175}  src="QR-code.png" /> 
                     </Box>
-                    <FigureImage  style={{ marginTop: "19px", paddingLeft: 70 }} width={375} height={300} alt="171x180" src="K-bank.png" /> 
+                    <FigureImage  style={{ marginTop: "19px", paddingLeft: 70 }} width={260} height={210} alt="171x180" src="K-bank.png" /> 
                     <Box display="flex" flexDirection="column" alignItems="center">
-                        <Typography style={{ fontSize: 24, textAlign: "center", fontWeight: "700", marginTop: "5px" }} color='black'>กรุณาโทรยืนยันเพื่อแจ้งการจอง</Typography>
-                        <Typography style={{ fontSize: 24, textAlign: "center", fontWeight: "700", marginTop: "5px" }} color='black'>และการชำระเงิน</Typography>
-                        <Typography style={{ fontSize: 24, textAlign: "center", fontWeight: "700", marginTop: "5px" }} color='black'>Tel : 020-599-6363</Typography>
-                        <br/>
+                        <Typography style={{ fontSize: 16, textAlign: "center", fontWeight: "700", marginTop: "5px" }} color='black'>กรุณาโทรยืนยันเพื่อแจ้งการจอง</Typography>
+                        <Typography style={{ fontSize: 16, textAlign: "center", fontWeight: "700", marginTop: "5px" }} color='black'>และการชำระเงิน</Typography>
+                        <Typography style={{ fontSize: 16, textAlign: "center", fontWeight: "700", marginTop: "5px", marginBottom: "10px" }} color='black'>Tel : 020-599-6363</Typography>
                     </Box>
                     </div>
                 </div>
