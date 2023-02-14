cda9279468bb98ff2e256afad7bb4993519d5e0c
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2023-02-14 23:45:55 +0700

    Make cardtour use data from fetch

--- client/src/components/CardTour.tsx
+++ client/src/components/CardTour.tsx
@@ -4,21 +4,28 @@ import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
 import Typography from '@mui/joy/Typography';
 import { useNavigate } from 'react-router-dom';
+import Tour from '../models/tour'
 
-function CardTour() {
+interface Props {
+  Tours : Tour
+}
+
+const CardTour = (props:Props) => {
   const navigate = useNavigate();
+  const item = props.Tours.attributes
+
+  const tourimg = `http://localhost:1337${item.image.data.attributes.formats.thumbnail.url}`
 
   return (
     <>
       <Card variant="outlined" sx={{ width: 250,backgroundColor: 'white' }}>
       <Typography level="h2" fontSize="xl" fontWeight="xl" color='primary' sx={{ mb: 0.5 }}>
-        เกาะสิมิลัน
+        {item.Title}
       </Typography>
       
       <AspectRatio minHeight="100px" maxHeight="150px" sx={{ my: 2 }}>
         <img
-          src="https://i.imgur.com/i2yEcg4.png"
-          srcSet="https://i.imgur.com/i2yEcg4.png 2x"
+          src={tourimg}
           loading="lazy"
           alt=""
         />
@@ -26,9 +33,9 @@ function CardTour() {
       <Box sx={{ display: 'flex' }}>
         <div>
           <Typography fontSize="lg" fontWeight="lg" color='success'>
-            ราคา 1,900 บาท/ท่าน
+            ราคา {item.price} บาท/ท่าน
           </Typography>
-          <Typography level="body3" color='danger'>เหลือ 3 ที่</Typography>
+          <Typography level="body3" color='danger'>เหลือ {item.available_seat} ที่</Typography>
         </div>
         <Button 
           href="similan"
