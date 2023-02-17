a0b92d9fb482c5eb919e7779f32c44e180eed453
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2023-02-17 21:26:28 +0700

    Create CardReview

---
+++ client/src/components/CardReview.tsx
@@ -0,0 +1,33 @@
+import Card from '@mui/joy/Card';
+import Avatar from  '@mui/joy/Avatar';
+import Typography from '@mui/material/Typography/Typography';
+import UserNavbar from '../components/UserNavbar';
+import StarIcon from '@mui/icons-material/Star';
+import {Row,Col,Container, } from 'react-bootstrap';
+
+function CardReview() {
+    return (
+        <div>
+            <Row>
+                <Card  variant="outlined" sx={{ width: 250 , marginLeft: 5, marginTop:5, backgroundColor:'white', border:3 }} size='lg'>
+                    <Row xs="2" style={{marginTop:"5px"}}>
+                        <Col xs='3'>
+                            <Avatar variant='outlined' sx={{}}>RE</Avatar>
+                            <StarIcon style={{color:"yellow",marginTop:"6px", marginLeft:'15'}}/>
+                        </Col>  
+                        <Col xs='6'>
+                            <Typography>re</Typography>
+                            <Typography>2/17/2023</Typography>
+                            <Typography style={{marginTop:"1px"}}>9/10 คะแนน</Typography>
+                        </Col>
+                    </Row>
+                    <Row style={{marginBottom:"15px"}}>
+                        <Typography>ชอบมาก อยากไปอีก บริการดี มากเลยครับ</Typography>
+                    </Row>
+                </Card>
+            </Row>
+        </div>
+    )
+}
+
+export default CardReview;
--- client/src/page/Review.tsx
+++ client/src/page/Review.tsx
@@ -1,10 +1,18 @@
-import UserNavbar from "../components/UserNavbar"
+import { Box } from '@mui/system';
+import Card from '@mui/joy/Card';
+import Avatar from  '@mui/joy/Avatar';
+import Typography from '@mui/material/Typography/Typography';
+import {Row,Col,Container, } from 'react-bootstrap';
+import FigureImage from 'react-bootstrap/FigureImage'
+import Button from '@mui/material/Button';
+import UserNavbar from '../components/UserNavbar';
+import StarIcon from '@mui/icons-material/Star';
+import CardReview from '../components/CardReview';
 
 const ReviewPage = () => {
     return (
         <div>
             <UserNavbar/>
-            <h1>Review Page</h1>
         </div>
     )
 }
