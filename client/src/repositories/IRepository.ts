c1323111ef5ef49d4090f930a0af4ff3ef10d8dd
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2023-02-18 23:25:16 +0700

    Add method in IRepo

--- client/src/repositories/IRepository.ts
+++ client/src/repositories/IRepository.ts
@@ -1,5 +1,8 @@
 export interface IRepository<T> {
-    getAll(): Promise<T[] | null>;
-    get?(id: string|number) : Promise<T[] | null>;
+    getAll?(): Promise<T[] | null>;
+    getTour?(id: string|number) : Promise<T[] | null>;
     getCategory?(type: string) : Promise<T[] | null>
+    getPayment?(user: string | number) : Promise<T[] | null>;
+    createPayment?(data: T): Promise<T>;
+    updateTour?(id: string|number, data : T) : Promise<T>;
 }
