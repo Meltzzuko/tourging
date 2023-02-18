54c9fbfc13c51beb907d9df2385d1908a4786676
นฤเบศร โสพรรณรัตน์ <bosschikung@gmail.com>
2023-02-18 16:23:09 +0700

    fix ReviewPage scroll horizontal

--- client/src/components/CardReview.tsx
+++ client/src/components/CardReview.tsx
@@ -4,28 +4,31 @@ import Typography from '@mui/material/Typography/Typography';
 import UserNavbar from '../components/UserNavbar';
 import StarIcon from '@mui/icons-material/Star';
 import {Row,Col,Container, } from 'react-bootstrap';
+import Box from '@mui/system/Box/Box';
 
 function CardReview() {
     return (
         <div>
-            <Row>
-                <Card  variant="outlined" sx={{ width: 250 , marginLeft: 5, marginTop:5, backgroundColor:'white', border:3 }} size='lg'>
-                    <Row xs="2" style={{marginTop:"5px"}}>
-                        <Col xs='3'>
-                            <Avatar variant='outlined' sx={{}}>RE</Avatar>
-                            <StarIcon style={{color:"yellow",marginTop:"6px", marginLeft:'15'}}/>
-                        </Col>  
-                        <Col xs='6'>
-                            <Typography>re</Typography>
-                            <Typography>2/17/2023</Typography>
-                            <Typography style={{marginTop:"1px"}}>9/10 คะแนน</Typography>
-                        </Col>
-                    </Row>
-                    <Row style={{marginBottom:"15px"}}>
-                        <Typography>ชอบมาก อยากไปอีก บริการดี มากเลยครับ</Typography>
-                    </Row>
-                </Card>
-            </Row>
+            <Box sx={{ display: 'flex' }}>
+                <Row>
+                    <Card  variant="outlined" sx={{ width: 250 , marginLeft: 15, marginTop:5, backgroundColor:'white', border:3 }} size='lg'>
+                        <Row xs="2" style={{marginTop:"5px"}}>
+                            <Col xs='3'>
+                                <Avatar variant='outlined' sx={{}}>RE</Avatar>
+                                <StarIcon style={{color:"yellow",marginTop:"6px", marginLeft:'15'}}/>
+                            </Col>  
+                            <Col xs='6'>
+                                <Typography>re</Typography>
+                                <Typography>2/17/2023</Typography>
+                                <Typography style={{marginTop:"1px"}}>9/10 คะแนน</Typography>
+                            </Col>
+                        </Row>
+                        <Row style={{marginBottom:"15px"}}>
+                            <Typography>ชอบมาก อยากไปอีก บริการดี มากเลยครับ</Typography>
+                        </Row>
+                    </Card>
+                </Row>
+            </Box>
         </div>
     )
 }
