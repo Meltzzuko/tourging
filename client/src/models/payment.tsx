2334d2836d4113ab1bee2ac9ff77778b3bb30f60
วรรณศักดิ์ หนูปลอด <you007543@gmail.com>
2023-02-18 23:34:27 +0700

    Create payment model

---
+++ client/src/models/payment.tsx
@@ -0,0 +1,12 @@
+export default interface payment {
+    data : {
+        status: boolean;
+        user : string;
+        tour_name : string;
+        tour_start : string;
+        tour_end : string | null;
+        quantity : number;
+        total_price : number;
+        image_url : string;
+    }
+}
