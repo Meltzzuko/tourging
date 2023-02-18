e1b5291020fdd54ca757e0b6ed4e9e8f444f51fb
Phattaraphon <6510110344@psu.ac.th>
2023-02-18 23:36:32 +0700

    change method main & add update tour in tourrepo

--- client/src/repositories/TourRepository.ts
+++ client/src/repositories/TourRepository.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
+import payment from "../models/payment";
 import Tours from "../models/tour";
+import Tourseat from "../models/tourseat";
 import { IRepository } from "./IRepository";
+import { userData } from "../helper";
 
-export class TourRepository implements IRepository<Tours>{
+const user = userData()
+
+export class TourRepository implements IRepository<Tours | Tourseat>{
     urlPrefix = "http://localhost:1337/api/user-tours?populate=*"
+    token = user.jwt
 
     async getAll(): Promise<Tours[] | null> {
         const resp = await fetch(`${this.urlPrefix}`);
@@ -11,7 +17,7 @@ export class TourRepository implements IRepository<Tours>{
         return data.data;
     }
 
-    async get(id: string): Promise<Tours[] | null> {
+    async getTour(id: string): Promise<Tours[] | null> {
         const resp = await fetch(`${this.urlPrefix}&filters[id][$eq]=${id}`);
         const data = await resp.json();
         return data.data;
@@ -23,4 +29,17 @@ export class TourRepository implements IRepository<Tours>{
         return data.data
     }
 
-  }
+    async updateTour(id: string | number, data: Tourseat): Promise<Tourseat> {
+        const resp = await fetch(`http://localhost:1337/api/user-tours/${id}`, {
+            method: "PUT",
+            headers: { 
+                "Authorization": `Bearer ${this.token}`,
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        const data_res = await resp.json();
+        return data_res
+    }
+}
