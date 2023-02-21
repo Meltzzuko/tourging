5f7c9785329878257d4603c05ea2c218cd798ad8
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2023-02-22 01:13:34 +0700

    enum review data to cardReview

--- client/src/components/CardReview.tsx
+++ client/src/components/CardReview.tsx
@@ -1,30 +1,38 @@
 import Card from '@mui/joy/Card';
-import Avatar from  '@mui/joy/Avatar';
+import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography/Typography';
 import UserNavbar from '../components/UserNavbar';
 import StarIcon from '@mui/icons-material/Star';
 import {Row,Col,Container, } from 'react-bootstrap';
 import Box from '@mui/system/Box/Box';
+import Review from '../models/review';
 
-function CardReview() {
+interface Props {
+    reviewData: Review
+}
+
+function CardReview(props: Props) {
+    const reviewData = props.reviewData ? props.reviewData.attributes : null
+    const avatar_img = reviewData?.avatar
+
     return (
         <div>
             <Box sx={{ display: 'flex' }}>
                 <Row>
-                    <Card  variant="outlined" sx={{ width: 250 , marginLeft: 15, marginTop:5, backgroundColor:'white', border:3 }} size='lg'>
+                    <Card  variant="outlined" sx={{ width: 270 , marginLeft: 15, marginTop:5, backgroundColor:'white', border:3 }} size='lg'>
                         <Row xs="2" style={{marginTop:"5px"}}>
                             <Col xs='3'>
-                                <Avatar variant='outlined'>RE</Avatar>
+                                <Avatar src={avatar_img}/>
                                 <StarIcon style={{color:"yellow",marginTop:"6px", marginLeft:'15'}}/>
                             </Col>  
                             <Col xs='6'>
-                                <Typography style={{fontWeight: "bold", color: "black"}}>Re</Typography>
-                                <Typography>2/17/2023</Typography>
-                                <Typography style={{marginTop:"1px"}}>9/10 คะแนน</Typography>
+                                <Typography style={{fontWeight: "bold", color: "black"}}>{reviewData?.author}</Typography>
+                                <Typography>{reviewData?.date}</Typography>
+                                <Typography style={{marginTop:"1px"}}>{reviewData?.score}/10 คะแนน</Typography>
                             </Col>
                         </Row>
                         <Row style={{marginBottom:"15px"}}>
-                            <Typography>ชอบมาก อยากไปอีก บริการดี มากเลยครับ</Typography>
+                            <Typography>{reviewData?.comment}</Typography>
                         </Row>
                     </Card>
                 </Row>
