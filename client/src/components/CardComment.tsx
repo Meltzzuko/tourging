9dab3c3ef072dc303f8311ce537836dfc13f54c9
ธรากร ชลายนคุปต์ <meowkhangban@gmail.com>
2023-02-24 00:33:14 +0700

    Can review only paid & delete review

--- client/src/components/CardComment.tsx
+++ client/src/components/CardComment.tsx
@@ -7,19 +7,22 @@ import TextField from '@mui/material/TextField';
 import { Button, Typography } from '@mui/material';
 import Repo from '../repositories';
 import Postreview from '../models/postreview';
-import { userData } from '../helper';
 import { format } from 'date-fns';
 import Tours from '../models/tour';
 
 interface Props {
     tourdata : Tours
+    user : {
+        username : string
+        avatar : string
+    }
 }
 
 function CardComment(props: Props) {
     const [commentText, setCommentText] = useState('');
     const [scoreReview, setScoreReview] = useState('1');
     const tour_data = props.tourdata ? props.tourdata : null
-    const user = userData();
+    const user = props.user
 
     const handleCommentText = (e : ChangeEvent<HTMLInputElement>) => {
         setCommentText(e.target.value);
@@ -51,59 +54,57 @@ function CardComment(props: Props) {
     }
 
     return(
-        <div>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                <Card variant="outlined" sx={{ width: 1100,backgroundColor: 'white',marginTop:'330px',  }}>
-                    <Container>
-                        <form onSubmit={handleSubmit}>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+            <Card variant="outlined" sx={{ width: 1100,backgroundColor: 'white'}}>
+                <Container>
+                    <form onSubmit={handleSubmit}>
+                        <Row>
+                            <Col xs={3} md={4}>
+                            <Typography style={{ fontSize: 22, fontWeight: "bold", color: "black", textAlignLast: "left",marginLeft:"20px",marginBottom:"10px"}}>โปรดให้คะแนนเกี่ยวกับประสบการณ์ที่คุณได้รับ</Typography>
                             <Row>
-                                <Col xs={3} md={4}>
-                                <Typography style={{ fontSize: 22, fontWeight: "bold", color: "black", textAlignLast: "left",marginLeft:"20px",marginBottom:"10px"}}>โปรดให้คะแนนเกี่ยวกับประสบการณ์ที่คุณได้รับ</Typography>
-                                <Row>
-                                    <Col xs="auto">
-                                        <StarIcon style={{color:"yellow", marginLeft: '10px',marginTop:'10px'}}/>
-                                    </Col>
-                                    <Col>
-                                    <Typography style={{ fontSize: 22, fontWeight: "bold", color: "black", textAlignLast: "left"}}>
-                                        คะแนนเต็ม 10 :  
-                                        <input 
-                                            type="number" 
-                                            min="1" 
-                                            max="10" 
-                                            placeholder='1'
-                                            value={scoreReview}
-                                            onChange={handleScoreReview}
-                                            style={{width: "50px",marginLeft:"10px"}}
-                                            required  
-                                        />
-                                        </Typography> 
-                                    </Col>
-                                </Row>
+                                <Col xs="auto">
+                                    <StarIcon style={{color:"yellow", marginLeft: '10px',marginTop:'10px'}}/>
                                 </Col>
-                                <Col xs={2} md={6}>
-                                <TextField
-                                    id="filled-multiline-static"
-                                    sx={{ width: 550 }}
-                                    multiline
-                                    rows={4}
-                                    label="เพิ่มความคิดเห็นของคุณ"
-                                    variant="filled"
-                                    value={commentText}
-                                    onChange={handleCommentText}
-                                    required
+                                <Col>
+                                <Typography style={{ fontSize: 22, fontWeight: "bold", color: "black", textAlignLast: "left"}}>
+                                    คะแนนเต็ม 10 :  
+                                    <input 
+                                        type="number" 
+                                        min="1" 
+                                        max="10" 
+                                        placeholder='1'
+                                        value={scoreReview}
+                                        onChange={handleScoreReview}
+                                        style={{width: "50px",marginLeft:"10px"}}
+                                        required  
                                     />
+                                    </Typography> 
                                 </Col>
-                                    <Col xs={3} md={2} sx={{ display: 'flex', justifyContent: 'center' }}>
-                                        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                                            <Button type="submit" variant="contained" color="primary" size="large" sx={{marginTop:"45px"}}>โพสต์</Button>
-                                        </Box>
-                                    </Col>
                             </Row>
-                        </form>
-                    </Container>
-                </Card>
-            </Box>
-        </div>
+                            </Col>
+                            <Col xs={2} md={6}>
+                            <TextField
+                                id="filled-multiline-static"
+                                sx={{ width: 550 }}
+                                multiline
+                                rows={4}
+                                label="เพิ่มความคิดเห็นของคุณ"
+                                variant="filled"
+                                value={commentText}
+                                onChange={handleCommentText}
+                                required
+                                />
+                            </Col>
+                                <Col xs={3} md={2} sx={{ display: 'flex', justifyContent: 'center' }}>
+                                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                                        <Button type="submit" variant="contained" color="primary" size="large" sx={{marginTop:"45px"}}>โพสต์</Button>
+                                    </Box>
+                                </Col>
+                        </Row>
+                    </form>
+                </Container>
+            </Card>
+        </Box>
     );
 }
 
--- client/src/page/Review.tsx
+++ client/src/page/Review.tsx
@@ -11,10 +11,16 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Grid } from '@mui/material';
 import Tours from '../models/tour';
+import { userData } from '../helper';
+import paymentStatus from '../models/paymentStatus';
+import * as React from 'react';
+
+const user = userData();
 
 const ReviewPage = () => {
   const [reviews, setReviews] = useState<Review[]>([]);
   const [tourdata, setTourdata] = useState<Tours[]>([]);
+  const [paymentData,setPaymentData] = useState<paymentStatus[]>([]);
 
   const params = useParams();
 
@@ -22,40 +28,50 @@ const ReviewPage = () => {
     try {
         const res = await Repo.Reviewdata.getReview(params.id as string);
         const res2 = await Repo.Tourdata.getTourById(params.id as string);
+        const res3 = await Repo.Paymentdata.getPayment(user.username.trimEnd())
         if(res) {
             setReviews(res)
         }
         if(res2) {
             setTourdata(res2)
         }
+        if(res3) {
+            setPaymentData(res3)
+        }
     } catch (error) {
         console.log(error)
     }
   }
+
+  const IsPaid = paymentData.filter(
+    payment => payment.attributes.status === true && payment.attributes.tour_name === tourdata[0].attributes.Title
+  );
 
   useEffect(() => {
       fetchData();
   },[params.id]);
 
   return (
-      <div style={{ minHeight: "100vh" }}>
+    <React.Fragment>
       <UserNavbar/>
       <Box sx={{display: 'flex' ,justifyContent: 'center'}}>
-        <Card variant="outlined" sx={{ width: 300 , marginLeft: 15, marginTop:4, backgroundColor:'white', border:3}}>
+        <Card variant="outlined" sx={{ width: 300 , marginTop:4, backgroundColor:'white', border:3}}>
           <Typography style={{ fontSize: 24, fontWeight: "bold", color: "black", textAlignLast: "center"}}>Review & Comment</Typography>
         </Card>
       </Box>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10}}>
+      <Grid container spacing={{ xs: 2, md: 0 }} columns={{ xs: 2, sm: 8, md: 12, lg: 15, xl: 10}} >
         {reviews.map((item, index) => 
-            <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-                <CardReview reviewData={item}/>
-            </Grid>
+          <Grid item xs={4} sm={4} md={4} lg={4} xl={4} key={index}>
+            <CardReview reviewData={item} user={user}/>
+          </Grid>
         )}
+        {Array.isArray(IsPaid) && IsPaid.length > 0 &&
+        <AppBar position="sticky" color='transparent' sx={{top: 'auto',bottom: 0}}>
+          <CardComment tourdata={tourdata[0]} user={user} />
+        </AppBar>
+      }         
       </Grid>
-      <AppBar position='sticky' color='transparent' sx={{ bottom: 0 }}>
-        <CardComment tourdata={tourdata[0]}/>
-      </AppBar>              
-    </div>
+    </React.Fragment>
   )
 }
 
