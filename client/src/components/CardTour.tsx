5c6d3882d3bd4da36815fd5030b223a54b0e099e
Phattaraphon <6510110344@psu.ac.th>
2023-02-23 23:07:29 +0700

    make Homepage and cardtour be responsive by css

--- client/src/components/CardTour.tsx
+++ client/src/components/CardTour.tsx
@@ -4,55 +4,42 @@ import Button from '@mui/joy/Button';
 import Card from '@mui/joy/Card';
 import Typography from '@mui/joy/Typography';
 import { useNavigate } from 'react-router-dom';
-import Tour from '../models/tour'
-
+import Tour from '../models/tour';
 interface Props {
-  Tours : Tour
+  Tours: Tour;
 }
-
-const CardTour = (props:Props) => {
+const CardTour = (props: Props) => {
   const navigate = useNavigate();
-  const item = props.Tours.attributes
-
-  const tourimg = `http://localhost:1337${item.image.data.attributes.formats.thumbnail.url}`
-
+  const item = props.Tours.attributes;
+  const tourimg = `http://localhost:1337${item.image.data.attributes.formats.thumbnail.url}`;
   return (
-    <>
-      <Card variant="outlined" sx={{ width: 250,backgroundColor: 'white' }}>
-      <Typography level="h2" fontSize="xl" fontWeight="xl" color='primary' sx={{ mb: 0.5 }}>
+    <Card variant="outlined" sx={{ width: '100%', maxWidth: 400,backgroundColor: 'white' }}>
+      <Typography level="h2" fontSize="xl" fontWeight="xl" color="primary" sx={{ mb: 0.5 }}>
         {item.Title}
       </Typography>
-      
       <AspectRatio minHeight="100px" maxHeight="150px" sx={{ my: 2 }}>
-        <img
-          src={tourimg}
-          loading="lazy"
-          alt=""
-        />
+        <img src={tourimg} loading="lazy" alt="" />
       </AspectRatio>
-      <Box sx={{ display: 'flex' }}>
-        <div>
-          <Typography fontSize="lg" fontWeight="lg" color='success'>
-            ราคา {item.price} บาท/ท่าน
-          </Typography>
-          <Typography level="body3" color='danger'>เหลือ {item.available_seat} ที่</Typography>
-        </div>
-      </Box>
-      <Button 
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography fontSize="lg" fontWeight="lg" color="success" sx={{ mt: 2 }}>
+          ราคา {item.price} บาท/ท่าน
+        </Typography>
+        <Typography level="body3" color="danger" sx={{ mt: 1, mb: 2 }}>
+          เหลือ {item.available_seat} ที่
+        </Typography>
+        <Button
           href="similan"
           variant="outlined"
           size="sm"
           color="primary"
           aria-label="Explore Bahamas Islands"
-          sx={{ ml: 'auto', fontWeight: 600, color:"purple"}}
+          sx={{ mb: 2, fontWeight: 600 }}
           onClick={() => navigate(`/detail/${props.Tours.id}`)}
         >
           รายละเอียด
         </Button>
+      </Box>
     </Card>
-    </>
-
   );
-}
-
+};
 export default CardTour;
--- client/src/page/Home.tsx
+++ client/src/page/Home.tsx
@@ -4,90 +4,75 @@ import CardTour from '../components/CardTour';
 import { useEffect,useState } from 'react';
 import Tour from '../models/tour';
 import { Grid } from '@mui/material';
-import Repo from '../repositories'
-
-
+import Repo from '../repositories';
+import '../Home.css';
 const Homepage = () => {
   const [tourdata, setTourData] = useState<Tour[]>([]);
-
   const fetchData = async () => {
     const res = await Repo.Tourdata.getAll()
     if(res) {
         setTourData(res)
     }
   }
-
   useEffect(() => {
     fetchData()
   }, [])
-
-    const onedaytrip = tourdata.filter(
-      tour => tour.attributes.category.data.attributes.type === "One-day"
-      );
-
-    const manydaytrip = tourdata.filter(
-      tour => tour.attributes.category.data.attributes.type === "Many-day"
-    )
-
-    return (
-        <div>
-            <UserNavbar/>
-            <Container>
-                <Row style={{ marginTop: "15px" }}>
-                    <Col md={{ span: 6, offset: 4 }} style={{ display: "flex" }}>
-                        <Button href='/genre/One-day' style={{ marginRight: "15px" , border: "2px solid black", borderRadius:"1.5rem", fontSize:"0.9rem" , boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.85)", backgroundColor:"rgb(29, 63, 92)" }}>ทัวร์ภูเก็ต One Day Trip</Button>
-                        <Button href='/genre/Many-day' style = {{border: "2px solid black", borderRadius:"1.5rem", fontSize:"0.9rem", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.85)", backgroundColor:"rgb(29, 63, 92)"}}>ทัวร์ภูเก็ตพร้อมที่พัก</Button>
-                    </Col>
-                </Row>
-                <Row style={{ marginTop: "15px" , marginBottom: '15px'}}>
-                    <Col xs={12} md={3} 
-                        style={{ 
-                            backgroundColor: "white", 
-                            borderRadius: "10px", 
-                            padding: "10px", 
-                            fontSize: '18px',
-                            fontWeight: "bold",
-                            textAlign: 'center',
-                            border:"2px solid black"  }}>
-                        ทัวร์ภูเก็ต One Day Trip
-                    </Col>
-                    <Col xs={6} md={9} style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
-                        <hr style={{ height: "2px", width: "80%", marginLeft: "5px", borderWidth: "5px",borderRadius:"20px", }} />
-                    </Col>
-                </Row>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10}}>
-                  {onedaytrip.map((item, index) => 
-                      <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-                          <CardTour Tours={item} />
-                      </Grid>
-                  )}
-                </Grid>
-                <Row style={{ marginTop: "15px" , marginBottom: '15px'}}>
-                    <Col xs={12} md={3} 
-                        style={{ 
-                            backgroundColor: "white", 
-                            borderRadius: "10px", 
-                            padding: "10px", 
-                            fontSize: '18px',
-                            fontWeight: "bold",
-                            textAlign: 'center',
-                            border:"2px solid black"  }}>
-                        ทัวร์ภูเก็ตพร้อมที่พัก
-                    </Col>
-                    <Col xs={6} md={9} style={{ display: "flex", alignItems: "center", justifyContent: "left" }}>
-                        <hr style={{ height: "2px", width: "80%", marginLeft: "5px", borderWidth: "5px",borderRadius:"20px" }} />
-                    </Col>
-                </Row>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10}}>
-                  {manydaytrip.map((item, index) => 
-                      <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-                          <CardTour Tours={item} />
-                      </Grid>
-                  )}
-                </Grid>
-            </Container>
-        </div>
-
+  const onedaytrip = tourdata.filter(
+    tour => tour.attributes.category.data.attributes.type === "One-day"
+  );
+  const manydaytrip = tourdata.filter(
+    tour => tour.attributes.category.data.attributes.type === "Many-day"
+  );
+  return (
+    <div className="homepage">
+      <UserNavbar/>
+      <Container>
+        <Row>
+          <center>
+          <Col md={{ span: 8, offset: 4 }} style={{ display: "flex" }}>
+            <Button href='/genre/One-day' className="homepage-button" style={{
+                border: "0.4vh solid black",marginTop:'3vh',marginBottom:'3vh',borderRadius:"1.5rem", fontSize:"0.9rem" , boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.85)", backgroundColor:"rgb(29, 63, 92)" }}>ทัวร์ภูเก็ต One Day Trip</Button>
+            <Button href='/genre/Many-day' className="homepage-button" style={{ marginLeft: '2vw',
+                border: "0.4vh solid black",marginTop:'3vh',marginBottom:'3vh',borderRadius:"1.5rem", fontSize:"0.9rem", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.85)", backgroundColor:"rgb(29, 63, 92)" }}>ทัวร์ภูเก็ตพร้อมที่พัก</Button>
+          </Col>
+          </center>
+        </Row>
+        <center>
+        <Row>
+          <Col xs={11} md={3} className="section-title"style={{ marginBottom:'3vh' }}>
+          <center>ทัวร์ภูเก็ต One Day Trip</center>
+          </Col>
+          <Col xs={5} md={9} className="section-line">
+            <hr />
+          </Col>
+        </Row>
+        </center>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10}}>
+          {onedaytrip.map((item, index) => 
+            <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
+              <CardTour Tours={item} />
+            </Grid>
+          )}
+        </Grid>
+        <center>
+      <Row style={{ marginTop: "15px" , marginBottom: '15px'}}>
+          <Col xs={11} md={3} className="section-title"style={{ marginBottom:'3vh' }}>
+            <center>ทัวร์ภูเก็ต พร้อมที่พัก</center>
+          </Col>
+          <Col xs={5} md={9} className="section-line">
+            <hr />
+          </Col>
+      </Row>
+      </center>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 2, sm: 8, md: 12, lg: 12, xl: 10}}>
+        {manydaytrip.map((item, index) => 
+          <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
+            <CardTour Tours={item} />
+          </Grid>
+        )}
+      </Grid>
+      </Container>
+    </div>
     )
 }
 export default Homepage;
